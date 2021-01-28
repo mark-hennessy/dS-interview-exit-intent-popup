6bc2a6299f2b6e369be0755f0a58fe0d1bd945ac
Mark Hennessy <markhennessy@deinestudienfinanzierung.de>
2021-01-28 14:58:01 +0100

    fix example test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import ReactDOM from "react-dom";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-// remove the <App /> added by index.js
-ReactDOM.unmountComponentAtNode(document.getElementById("root"));
-
 describe("App", () => {
   it("renders without crashing", () => {
     render(<App shouldShowModalInitially />);
-    expect(screen.getByText("Lorem Ipsum")).toBeInTheDocument();
+    expect(screen.getByText("Sicher dir jetzt das Paket für dein Studium")).toBeInTheDocument();
   });
 
   it("opens modal on first exit intent", () => {});
--- src/ExitModal.test.js
+++ src/ExitModal.test.js
@@ -1,12 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import ReactDOM from "react-dom";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ExitModal from "./ExitModal";
-
-// remove the <App /> added by index.js
-ReactDOM.unmountComponentAtNode(document.getElementById("root"));
 
 describe("ExitModal", () => {
   it("renders without crashing", () => {});
